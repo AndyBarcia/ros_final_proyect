8584106fe71f997717705ae6dc53b1df3fb7384c
AndyBarcia <andybarcia4@gmail.com>
2025-01-05 14:33:45 +0100

    Update tree with rotations

--- src/main.py
+++ src/main.py
@@ -24,28 +24,42 @@ def main():
     
     # Behavior Tree Structure
     root = py_trees.composites.Selector("Root", memory=True, children=[
+        # Repeatedly move forward as long as there is no obstacle.
         py_trees.composites.Sequence(
             "MoveForward", 
-            memory=False, # Important no memory to force to execute repeatedly.
+            memory=False,
             children=[
                 ObstacleDetection(robot_vel_pub, fixed_direction_angle=0.0), 
                 FixedMovement(robot_vel_pub, linear_speed=0.2)
             ]
         ),
+        # Otherwise, try to move backwards and rotate randomly.
         py_trees.composites.Sequence(
             "RetryDirection", 
-            memory=False, # Important no memory to force to execute repeatedly.
+            memory=True,
             children=[
-                # How to optionally move backwards and rotate?
-                py_trees.composites.Sequence(
-                    "MoveBackwards",
-                    memory=False,
-                    children = [
-                        ObstacleDetection(robot_vel_pub, fixed_direction_angle=180.0),
-                        FixedMovement(robot_vel_pub, linear_speed=-0.2, max_time=1.0),
-                    ]
+                # Optionally try to move backwards. If not possible, continue.
+                py_trees.decorators.FailureIsSuccess(
+                    "TryBackoff",
+                    py_trees.composites.Sequence(
+                        "Backoff",
+                        memory=False,
+                        children = [
+                            # Try to move backwards a fixed ammount of time.
+                            ObstacleDetection(robot_vel_pub, fixed_direction_angle=180.0),
+                            py_trees.decorators.Timeout(
+                                "MoveBackwards",
+                                duration=1.0,
+                                child=FixedMovement(robot_vel_pub, linear_speed=-0.2),
+                            )
+                        ]
+                    )
                 ),
-                RandomSafeRotation(robot_vel_pub)
+                # Temperature specifies how biased is the robot towards open spaces.
+                # The lower, the more likely it is the direction is a big open space.
+                # Note that low values could result in the robot moving back and forth
+                # in big closed spaces.
+                RandomSafeRotation(robot_vel_pub, temperature=0.1)
             ]
         ),
     ])
