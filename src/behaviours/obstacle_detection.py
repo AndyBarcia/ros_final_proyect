Fix random rotation

src/behaviours/obstacle_detection.py
@@ -9,7 +9,7 @@ class ObstacleDetection(py_trees.behaviour.Behaviour):
         robot_vel_pub, 
         fixed_direction_angle=0.0,
         name="ObstacleDetection", 
-        obstacle_threshold=0.5
+        obstacle_threshold=0.15
     ):
         super(ObstacleDetection, self).__init__(name)
 
@@ -29,15 +29,21 @@ class ObstacleDetection(py_trees.behaviour.Behaviour):
             closest_index = np.argmin(np.abs(np.array(self.blackboard.sensor_angles) - angle))
             return self.blackboard.safe_distances[closest_index]
         else:
-            # Otherwise, assume the safe distance is 0.
-            return 0
+            # Otherwise, report that we don't know the safe distance.
+            return None
 
     def update(self):
         # Get safe distance along given forward direction.
         forward_safe_distance = self.get_max_distance_in_direction(self.fixed_direction_angle)
+        
+        # If we still don't know the safe distance because LIDAR didn't boot up, keep running.
+        if not forward_safe_distance:
+            return py_trees.common.Status.RUNNING
 
         if forward_safe_distance < self.obstacle_threshold:
-            print("OBSTACLE DETECTED")
+            print(f"OBSTACLE DETECTED AT DIRECTION {self.fixed_direction_angle}; DISTANCE {forward_safe_distance}")
+
+            print(self.blackboard.safe_distances)
 
             # Stop the robot
             twist = Twist()
@@ -48,7 +54,5 @@ class ObstacleDetection(py_trees.behaviour.Behaviour):
             # Fail so that no other behaviours in the tree get executed.
             return py_trees.common.Status.FAILURE
         else:
-            print("MOVING")
-
             # If no obstacle, we can continue.
             return py_trees.common.Status.SUCCESS

src/behaviours/random_rotation.py
@@ -3,8 +3,8 @@ import py_trees
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
-import random
 import numpy as np
+import math
 
 class RandomSafeRotation(py_trees.behaviour.Behaviour):
     def __init__(
@@ -12,48 +12,72 @@ class RandomSafeRotation(py_trees.behaviour.Behaviour):
         robot_vel_pub,
         angular_speed=0.3,
         name="RandomRotation",
-        obstacle_threshold=0.5,
-        angle_threshold=0.1  # Threshold for considering target reached (radians)
+        obstacle_threshold=0.01,
+        angle_threshold=0.1,  # Threshold for considering target reached (radians)
+        temperature=1.0       # Temperature parameter for softmax
     ):
         super(RandomSafeRotation, self).__init__(name)
         self.blackboard = py_trees.blackboard.Client(name="Global")
         self.blackboard.register_key(key="safe_distances", access=py_trees.common.Access.READ)
         self.blackboard.register_key(key="sensor_angles", access=py_trees.common.Access.READ)
-        
         self.robot_vel_pub = robot_vel_pub
         self.angular_speed = angular_speed
         self.angle_threshold = angle_threshold
         self.obstacle_threshold = obstacle_threshold
+        self.temperature = temperature
         
         # Subscribe to odometry to get robot's orientation
         self.odom_sub = rospy.Subscriber('/odom', Odometry, self.odom_callback)
         self.current_yaw = 0.0
         self.target_safe_angle = None
-        
-    def generate_random_safe_distance(self):
+
+    def generate_random_safe_direction(self):
         safe_distances = np.array(self.blackboard.safe_distances)
         safe_angles = np.array(self.blackboard.sensor_angles)
-        safe_mask = safe_distances > self.obstacle_threshold
         
-        if len(safe_angles) == 0:
-            self.target_safe_angle = None
-            return False
+        # Filter out unsafe distances
+        safe_mask = safe_distances > self.obstacle_threshold
+        safe_distances[~safe_mask] = -math.inf
 
-        # Random safe angle
-        self.target_safe_angle = np.random.choice(safe_angles[safe_mask]) + self.current_yaw
+        # If no safe distance, abort.
+        if sum(safe_mask) == 0:
+            return None
+        
+        # Clip maxium distance so that they aren't always picked.
+        # safe_distances = np.clip(safe_distances, a_min=None, a_max=self.max_distance)
+                
+        # Apply softmax with temperature
+        exp_distances = np.exp(safe_distances / self.temperature)
+        probabilities = exp_distances / np.sum(exp_distances)
+        
+        # Choose angle based on calculated probabilities
+        return np.random.choice(safe_angles, p=probabilities) + self.current_yaw
+
+    def initialise(self):
+        # Try to generate a random safe direction at start.
+        self.target_safe_angle = self.generate_random_safe_direction()
+
+    def terminate(self, new_status: py_trees.common.Status):
+        # Stop rotation
+        twist = Twist()
+        twist.angular.z = 0.0
+        self.robot_vel_pub.publish(twist)
+        
+        # Reset target angle
+        self.target_safe_angle = None
 
     def odom_callback(self, msg):
         # Get orientation from quaternion
         orientation = msg.pose.pose.orientation
         orientation_list = [orientation.x, orientation.y, orientation.z, orientation.w]
         _, _, self.current_yaw = euler_from_quaternion(orientation_list)
-        
+
     def update(self):
         if self.target_safe_angle is None:
-            if not self.generate_random_safe_distance():
+            self.target_safe_angle = self.generate_random_safe_direction()
+            if not self.target_safe_angle:
                 print("NO SAFE ANGLES")
                 return py_trees.common.Status.FAILURE
-            print("NO SAFE ANGLES")
         
         # Calculate relative angle to target
         relative_angle = self.target_safe_angle - self.current_yaw
@@ -63,6 +87,8 @@ class RandomSafeRotation(py_trees.behaviour.Behaviour):
             relative_angle -= 2 * np.pi
         while relative_angle < -np.pi:
             relative_angle += 2 * np.pi
+            
+        print(f"ROTATING TOWARDS {relative_angle}")
         
         # Check if target rotation is reached
         reached_target_rotation = abs(relative_angle) < self.angle_threshold
@@ -77,12 +103,4 @@ class RandomSafeRotation(py_trees.behaviour.Behaviour):
             self.robot_vel_pub.publish(twist)
             return py_trees.common.Status.RUNNING
         else:
-            # Stop rotation
-            twist = Twist()
-            twist.angular.z = 0.0
-            self.robot_vel_pub.publish(twist)
-
-            # Reset targets
-            self.target_safe_angle = None
-
             return py_trees.common.Status.SUCCESS